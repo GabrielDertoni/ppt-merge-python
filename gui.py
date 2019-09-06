fe92523ec230d687fa0f6244128415666224a80f
Gabriel Dertoni <gab.dertoni@gmail.com>
2019-09-06 02:00:03 -0300

    Added method to select multiple files and chage file order after selection

--- gui.py
+++ gui.py
@@ -11,26 +11,45 @@ root.geometry("700x700")
 root.iconbitmap(os.path.abspath("icon.ico"))
 
 vars = []
+presentation_entries = []
 
 def input_layout(frame, var, deletable=True):
-  container_frame = Frame(frame)
-  container_frame.pack(side=TOP, fill=X)
-  label_frame = Frame(container_frame)
+  vars_len = len(vars)
+  background = "lightgrey" if (vars_len + 1) % 2 == 0 else "SystemButtonFace"
+  container_frame = Frame(frame, bg=background, pady="5", padx="5")
+  container_frame.pack(side=TOP, fill=X, pady="5", padx="5")
+  label_frame = Frame(container_frame, bg=background)
   label_frame.pack(side=TOP, fill=X)
-  entry_frame = Frame(container_frame)
+  entry_frame = Frame(container_frame, bg=background)
   entry_frame.pack(side=TOP, fill=X)
+  arrows_frame = Frame(entry_frame, bg=background)
+  arrows_frame.pack(side=LEFT, fill=Y, padx="5")
 
-  label = Label(label_frame, text="Presentation path", font="Helvetica 10 bold")
+  def swap_vars(direction):
+    if direction == "up" and vars_len - 1 >= 0:
+      buff = var.get()
+      var.set(vars[vars_len - 1].get())
+      vars[vars_len - 1].set(buff)
+    elif direction == "down" and vars_len + 1 < len(vars):
+      buff = var.get()
+      var.set(vars[vars_len + 1].get())
+      vars[vars_len + 1].set(buff)
+
+  up_arrow = Button(arrows_frame, text="↑", command=lambda: swap_vars("up"), width="3")
+  down_arrow = Button(arrows_frame, text="↓", command=lambda: swap_vars("down"), width="3")
+  up_arrow.pack(side=TOP)
+  down_arrow.pack(side=BOTTOM)
+
+  label = Label(label_frame, text="Presentation path", bg=background, font="Helvetica 10 bold")
   label.pack(side=LEFT)
 
   entry = Entry(entry_frame, textvariable=var, width="30")
   def browse_dialog():
-    filename = filedialog.askopenfilename(initialdir="/", title="Select a presentation", filetype=[("PowerPoint files", "*.pptx")])
+    filename = filedialog.askopenfilename(title="Select a presentation", filetype=[("PowerPoint files", "*.pptx")])
     var.set(filename)
     entry.xview_moveto(1)
   
   browse_btn = Button(entry_frame, text="Browse", command=browse_dialog)
-  
   entry.pack(side=LEFT)
 
   # If the input is deletable, create a button to do that and pack it to the right, after the browse button.
@@ -43,6 +62,7 @@ def input_layout(frame, var, deletable=True):
     delete_btn.pack(side=RIGHT)
   
   browse_btn.pack(side=RIGHT)
+  return entry
 
 def print_info(vars):
   for var in vars:
@@ -50,8 +70,9 @@ def print_info(vars):
 
 def add_input(frame, deletable=True):
   var = StringVar()
+  entry = input_layout(frame, var, deletable=deletable)
   vars.append(var)
-  input_layout(frame, var, deletable=deletable)
+  presentation_entries.append(entry)
 
 def check_file(filepath, exists=True, extention=".pptx"):
   if not exists:
@@ -108,6 +129,7 @@ def save_dialog():
   filename = filedialog.asksaveasfilename(initialdir="/", title="Save as...", filetype=[("PowerPoint files", "*.pptx")])
   if os.path.splitext(filename)[1] == '': filename += '.pptx'
   output_path.set(filename)
+  output_entry.xview_moveto(1)
 
 output_browse = Button(output_entry_frame, text="Browse", command=save_dialog)
 output_browse.pack(side=RIGHT)
@@ -138,9 +160,33 @@ input_frame.bind("<Configure>", update_canvas)
 add_input(input_frame, deletable=False)
 add_input(input_frame, deletable=False)
 
+add_btn_frame = Frame(root)
+add_btn_frame.pack(side=TOP)
+
 # Button to add a new presentation to merge
-add_btn = Button(root, text="Add presentation", command=lambda: add_input(input_frame), bg="#d04424", fg="white")
-add_btn.pack(side=TOP, pady="15")
+add_btn = Button(add_btn_frame, text="Add presentation", command=lambda: add_input(input_frame), bg="#d04424", fg="white")
+add_btn.pack(side=LEFT, pady="10", padx="5")
+
+def browse_many_dialog():
+  filenames = filedialog.askopenfilenames(initialdir="/", title="Select presentations", filetype=[("PowerPoint files", "*.pptx")])
+  if len(vars) == 2 and vars[0].get() == "" and vars[1].get() == "" and len(filenames) >= 2:
+    for i in range(len(filenames)):
+      if i < 2:
+        vars[i].set(filenames[i])
+        presentation_entries[i].xview_moveto(1)
+      else:
+        add_input(input_frame)
+        vars[-1].set(filenames[i])
+        presentation_entries[-1].xview_moveto(1)
+  else:
+    for fname in filenames:
+      add_input(input_frame)
+      vars[-1].set(fname)
+      presentation_entries[-1].xview_moveto(1)
+  
+
+browse_btn = Button(add_btn_frame, text="Select files", command=browse_many_dialog, bg="#d04424", fg="white")
+browse_btn.pack(side=RIGHT, pady="10", padx="5")
 
 # Label where all errors are displayed
 error_frame = Frame(root)
